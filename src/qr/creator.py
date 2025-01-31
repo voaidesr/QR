f02c1b8890f28abf2c0e45bd22ad7a741292f43b
Florin Aioanei <122646871+aioanei@users.noreply.github.com>
2025-01-31 17:51:18 +0200

    completed apply_changes function

--- src/qr/creator.py
+++ src/qr/creator.py
@@ -142,9 +142,193 @@ class QR_base:
                 break
 
             group_idx += 1
+    def apply_mask(self, mask_idx:int):
 
-
-
+        if mask_idx == 1:
+            self.mask1()
+        elif mask_idx == 2:
+            self.mask2()
+        elif mask_idx == 3:
+            self.mask3()
+        elif mask_idx == 4:
+            self.mask4()
+        elif mask_idx == 5:
+            self.mask5()
+        elif mask_idx == 6:
+            self.mask6()
+        elif mask_idx == 7:
+            self.mask7()
+        elif mask_idx == 8:
+            self.mask8()
+        
+    def mask1(self):
+            a = np.zeros((25,25), dtype=int)
+            f = open('./res/function_patterns.txt')
+            i = 0
+            for line in f.readlines():
+                j = 0
+                for val in line.split():
+                    a[i,j] = int(val)
+                    j += 1
+                i += 1
+            # Iterate through matrix
+            for i in range(self.qr_matrix.shape[0]):
+                for j in range(self.qr_matrix.shape[1]):
+                    if self.qr_matrix[i,j] in [0,1] and (i + j) % 2 == 0 and a[i,j] == 4:
+                        self.qr_matrix[i,j] = 1 - self.qr_matrix[i,j]
+            self.qr_matrix[8,2] = 0
+            self.qr_matrix[8,3] = 1
+            self.qr_matrix[8,4] = 0
+            self.qr_matrix[22,8] = 0
+            self.qr_matrix[21,8] = 1
+            self.qr_matrix[20,8] = 0
+    def mask2(self):
+        a = np.zeros((25,25), dtype=int)
+        f = open('./res/function_patterns.txt')
+        i = 0
+        for line in f.readlines():
+            j = 0
+            for val in line.split():
+                a[i,j] = int(val)
+                j += 1
+            i += 1
+            # Iterate through matrix
+        for i in range(self.qr_matrix.shape[0]):
+            for j in range(self.qr_matrix.shape[1]):
+                if self.qr_matrix[i,j] in [0,1] and j % 2 == 0 and a[i,j] == 4:
+                    self.qr_matrix[i,j] = 1 - self.qr_matrix[i,j]
+            self.qr_matrix[8,2] = 0
+            self.qr_matrix[8,3] = 1
+            self.qr_matrix[8,4] = 1
+            self.qr_matrix[22,8] = 0
+            self.qr_matrix[21,8] = 1
+            self.qr_matrix[20,8] = 1
+    def mask3(self):
+        a = np.zeros((25,25), dtype=int)
+        f = open('./res/function_patterns.txt')
+        i = 0
+        for line in f.readlines():
+            j = 0
+            for val in line.split():
+                a[i,j] = int(val)
+                j += 1
+            i += 1
+            # Iterate through matrix
+        for i in range(self.qr_matrix.shape[0]):
+            for j in range(self.qr_matrix.shape[1]):
+                if self.qr_matrix[i,j] in [0,1] and j % 3 == 0 and a[i,j] == 4:
+                    self.qr_matrix[i,j] = 1 - self.qr_matrix[i,j]
+        self.qr_matrix[8,2] = 0
+        self.qr_matrix[8,3] = 0
+        self.qr_matrix[8,4] = 0
+        self.qr_matrix[22,8] = 0
+        self.qr_matrix[21,8] = 0
+        self.qr_matrix[20,8] = 0
+    def mask4(self):
+        a = np.zeros((25,25), dtype=int)
+        f = open('./res/function_patterns.txt')
+        i = 0
+        for line in f.readlines():
+            j = 0
+            for val in line.split():
+                a[i,j] = int(val)
+                j += 1
+            i += 1
+            # Iterate through matrix
+        for i in range(self.qr_matrix.shape[0]):
+            for j in range(self.qr_matrix.shape[1]):
+                if self.qr_matrix[i,j] in [0,1] and (i+j) % 3 == 0 and a[i,j] == 4:
+                    self.qr_matrix[i,j] = 1 - self.qr_matrix[i,j]
+        self.qr_matrix[8,2] = 0
+        self.qr_matrix[8,3] = 0
+        self.qr_matrix[8,4] = 1
+        self.qr_matrix[22,8] = 0
+        self.qr_matrix[21,8] = 0
+        self.qr_matrix[20,8] = 1
+    def mask5(self):
+        a = np.zeros((25,25), dtype=int)
+        f = open('./res/function_patterns.txt')
+        i = 0
+        for line in f.readlines():
+            j = 0
+            for val in line.split():
+                a[i,j] = int(val)
+                j += 1
+            i += 1
+            # Iterate through matrix
+        for i in range(self.qr_matrix.shape[0]):
+            for j in range(self.qr_matrix.shape[1]):
+                if self.qr_matrix[i,j] in [0,1] and ((int(i/2) + int(j/3)) % 2) == 0 and a[i,j] == 4:
+                    self.qr_matrix[i,j] = 1 - self.qr_matrix[i,j]
+        self.qr_matrix[8,2] = 1
+        self.qr_matrix[8,3] = 1
+        self.qr_matrix[8,4] = 0
+        self.qr_matrix[22,8] = 1
+        self.qr_matrix[21,8] = 1
+        self.qr_matrix[20,8] = 0
+    def mask6(self):
+        a = np.zeros((25,25), dtype=int)
+        f = open('./res/function_patterns.txt')
+        i = 0
+        for line in f.readlines():
+            j = 0
+            for val in line.split():
+                a[i,j] = int(val)
+                j += 1
+            i += 1
+            # Iterate through matrix
+        for i in range(self.qr_matrix.shape[0]):
+            for j in range(self.qr_matrix.shape[1]):
+                if self.qr_matrix[i,j] in [0,1] and (((i*j) % 2) + ((i*j) % 3)) == 0 and a[i,j] == 4:
+                    self.qr_matrix[i,j] = 1 - self.qr_matrix[i,j]
+        self.qr_matrix[8,2] = 1
+        self.qr_matrix[8,3] = 1
+        self.qr_matrix[8,4] = 1
+        self.qr_matrix[22,8] = 1
+        self.qr_matrix[21,8] = 1
+        self.qr_matrix[20,8] = 1
+    def mask7(self):
+        a = np.zeros((25,25), dtype=int)
+        f = open('./res/function_patterns.txt')
+        i = 0
+        for line in f.readlines():
+            j = 0
+            for val in line.split():
+                a[i,j] = int(val)
+                j += 1
+            i += 1
+            # Iterate through matrix
+        for i in range(self.qr_matrix.shape[0]):
+            for j in range(self.qr_matrix.shape[1]):
+                if self.qr_matrix[i,j] in [0,1] and ((((i*j) % 2) + ((i*j) % 3))%2) == 0 and a[i,j] == 4:
+                    self.qr_matrix[i,j] = 1 - self.qr_matrix[i,j]
+        self.qr_matrix[8,2] = 1
+        self.qr_matrix[8,3] = 0
+        self.qr_matrix[8,4] = 0
+        self.qr_matrix[22,8] = 1
+        self.qr_matrix[21,8] = 0
+        self.qr_matrix[20,8] = 0
+    def mask8(self):
+        a = np.zeros((25,25), dtype=int)
+        f = open('./res/function_patterns.txt')
+        i = 0
+        for line in f.readlines():
+            j = 0
+            for val in line.split():
+                a[i,j] = int(val)
+                j += 1
+            i += 1
+            # Iterate through matrix
+        for i in range(self.qr_matrix.shape[0]):
+            for j in range(self.qr_matrix.shape[1]):
+                if self.qr_matrix[i,j] in [0,1] and (((i+j) % 2) + ((i*j) % 3)) == 0 and a[i,j] == 4:
+                    self.qr_matrix[i,j] = 1 - self.qr_matrix[i,j]
+        self.qr_matrix[8,2] = 1
+        self.qr_matrix[8,3] = 0
+        self.qr_matrix[8,4] = 1
+        self.qr_matrix[22,8] = 1
+        self.qr_matrix[21,8] = 0
+        self.qr_matrix[20,8] = 1
     def get_matrix(self) -> np.array:
         return self.qr_matrix
     
@@ -153,11 +337,13 @@ from qr.visualizer import QR_Visualizer
 def test():
     base = QR_base(np.zeros((25,25)))
 
-    encoder = Encoder('Hello, World!')
+    encoder = Encoder('www.youtube.com/veritasium')
     e = encoder.get_encoded()
 
     base.load_stream_in_qr(e)
 
+    base.apply_mask(2)
+    
+
     interface = QR_Visualizer(base)
     interface.save_image()
-    
